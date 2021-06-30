7b646b7dab8c0d1abfeef19bb3692f3c935d2ab6
Prayas Banerjee <rupanreal7@gmail.com>
2021-06-30 22:58:15 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Auth } from 'aws-amplify';
+
 
 function App() {
+
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log(user);
+  }
+
+  async function signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => Auth.federatedSignIn({ provider: "Google" })}>Sign In with Google</button>
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={checkUser}>Check User</button>
+        <button onClick={signOut}>Logout</button>
     </div>
   );
 }
